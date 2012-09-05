b589431a9a297ba0d0486965b862c9df0a1c40df
David Shaw <dedshaw1612@gmail.com>
2012-09-04 20:44:09 -0400

    Gradient works, and is awesome!
    crashes at the end though :(

--- DavidShaw_HW1/src/DavidShaw_HW1App.cpp
+++ DavidShaw_HW1/src/DavidShaw_HW1App.cpp
@@ -15,22 +15,22 @@ class DavidShaw_HW1App : public AppBasic {
   public:
 	void setup();
 	void mouseDown( MouseEvent event );	
+	void keyDown(KeyEvent event);
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
 	void drawSquare(uint8_t* pixels,int x, int y);
-	//void DavidShaw_HW1App::drawSquare(uint8_t* pixels,int centerX, int centerY);
-
+	void blur(uint8_t* pixels);
+	
 private:
-	Surface* mySurface_; //The Surface object whose pixel array we will modify
-	//gl::Texture* myTexture_; //The Texture object that we use to display our Surface 
+	Surface* mySurface_; //The Surface object whose pixel array we will modify 
 	uint8_t* dataArray;
+	uint8_t* copy;
 	Surface space_Marine;
+	int gradCount;
+	int redCount;
 	
 };
-
-	//uint8_t* myPixels;
-	
 	//Width and height of the screen
 	static const int kAppWidth=800;
 	static const int kAppHeight=600;
@@ -40,34 +40,57 @@ void DavidShaw_HW1App::setup()
 
 {
 	mySurface_ = new Surface(kTextureSize,kTextureSize,false);
-	//myTexture_ = new gl::Texture(*mySurface_);
+
+	gradCount = 0;
+	rowCount = 0;
+	redCount = 255;
 
 	space_Marine = (loadImage( loadResource(RES_HAMMER) ));
-	//gl::Texture space_Marine(loadImage( loadResource(RES_HAMMER) ));
 	dataArray = (*mySurface_).getData();
+	copy = (*mySurface_).getData();
 	uint8_t* sm = space_Marine.getData();
 	for (int i = 0; i < 1024*1024*3; i++) {
 		dataArray[i] = sm[i];
+		copy[i] = dataArray[i];
 	}
-
 }
 
 void DavidShaw_HW1App::prepareSettings(Settings* settings) {
-//	(*settings).setWindowSize(kAppWidth,kAppHeight);
-	(*settings).setWindowSize(kTextureSize,kTextureSize);
+	(*settings).setWindowSize(kAppWidth,kAppHeight);
+	//(*settings).setWindowSize(kTextureSize,kTextureSize);
 	(*settings).setResizable(false);
 }
 
 void DavidShaw_HW1App::drawSquare(uint8_t* pixels,int posX, int posY) {
 
-	int startX = posY*3*1024;
-	int startY = posX*3;
+	
+	int startX = posX*3;
+	int startY = posY*3*1024;
 	for (int i = startX+startY;i < (startX + startY+ 150); i+=3) {
 		pixels[i] = 255;
 		pixels[i + 1] = 255;
 		pixels[i + 2] = 0;
+
+		//pixels[i*150*1024] = 255;
+		//pixels[i*150*1024 + 1] = 255;
+		//pixels[i*150*1024 + 2] = 0;
 	}
 
+}
+
+void DavidShaw_HW1App::blur(uint8_t* pixels) {
+	//Not a pretty method of doing it, nor does it work, but the plan
+	//was to average the red ints of all the neighboring pixels, the blue
+	//and the green. The copy is made before this method so it can be used
+	//for the actual surface when that one changes. For now it was coded to 
+	//only blur everything but a 1 pixel thick frame to avoid boundary math for now.
+	for(int i = 1024*3 + 3; i < 1024*1023*3 - 3; i++) {
+		pixels[i] = (copy[i] + copy[i+3] + copy[i+6] + 
+					 copy[i*1024*3] + copy[i*1024*3 + 3] + copy[i*1024*3 + 6] +
+					 copy[i*1024*6] + copy[i*1024*6 + 3] + copy[i*1024*6 + 6])/9;
+
+
+}
 }
 
 void DavidShaw_HW1App::mouseDown( MouseEvent event )
@@ -76,20 +99,35 @@ void DavidShaw_HW1App::mouseDown( MouseEvent event )
 		drawSquare(dataArray,event.getX(), event.getY());
 }
 
+
+void DavidShaw_HW1App::keyDown(KeyEvent event) {
+
+	/*
+	if(event.getCode() == event.KEY_b)
+		blur(dataArray);
+	*/
+}
+
 void DavidShaw_HW1App::update()
 {
-	//uint8_t* dataArray = (*mySurface_).getData();
-		
-	//(*myTexture_).update(*mySurface_,(*mySurface_).getBounds());
-		
+	gradCount++;
+	redCount--;
+
+	for(int i = gradCount*3*1024; i < (gradCount+1)*1024*3;i+=3) {
+		dataArray[i] = redCount;
+		dataArray[i + 1] = 0;
+		dataArray[i + 2] = 0;
+	}
+
+	if(gradCount > 1024*1024*3)
+		gradCount = 0;
+
 }
+
 
 void DavidShaw_HW1App::draw()
 {
-	//gl::draw(space_Marine);
 	gl::draw(*mySurface_);
-	//gl::draw(*myTexture_);
-	
 }
 
 CINDER_APP_BASIC( DavidShaw_HW1App, RendererGl )
